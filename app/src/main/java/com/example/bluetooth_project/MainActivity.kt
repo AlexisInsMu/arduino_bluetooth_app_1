dcd3c8a662ed1438a415f9486e085da896f115b5
alexisMuBa <alexis8.pl@outlook.com>
2022-08-05 21:14:24 -0500

    "V1.1.0 Aggregate Slash"

--- app/src/main/java/com/example/bluetooth_project/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth_project/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.bluetooth_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash: SplashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //se puede borrar
+
+        screenSplash.setKeepOnScreenCondition {true}
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
